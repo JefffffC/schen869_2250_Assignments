a800242d43896cc0964f3b0509562a8a78733481
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2019-03-19 09:42:42 -0400

    Added Assignment 3
    Created new repo for all three assignments and added assignment 3

---
+++ Assignment 3/Assets/__Scripts/SocialButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SocialButton : MonoBehaviour
+{
+
+    public Button socialButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socialButton.onClick.AddListener(ShareToTwitter);
+    }
+
+
+    // Social Media Share Function, to be called on game over (in function below)
+    public void ShareToTwitter()
+    {
+        string urlString = "http://twitter.com/home?status=";
+        urlString += "Check%20out%20my%20score%20of%20";
+        urlString += ScoreManager.SCORE;
+        urlString += "%20on%20Prospector%20Solitaire!";
+        Application.OpenURL(urlString);
+    }
+}
